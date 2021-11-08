917eb3151e8fbd47ecbfcf1cec50a7cc412a1537
andmalt <andrea.maltempi@gmail.com>
2021-11-08 18:10:16 +0100

    inserted comic model on the home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +14,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('index');
+        $comics = Comic::all();
+        // dd($comics);
+        
+        return view('index', compact('comics'));
     }
 
     /**
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,3 +2,32 @@
 
 @section('title', 'Home')
 
+@section('content')
+
+
+<div class="container p-5">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-10 col-lg-8">
+            <table class="table table-dark table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col-3">#</th>
+                        <th scope="col-3"> Titolo </th>
+                        <th scope="col-3">Autore</th>
+                        <th scope="col-3">Data di pubblicazione</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr> 
+                </tbody> 
+            </table>
+        </div>
+    </div>
+</div>
+
+@endsection
