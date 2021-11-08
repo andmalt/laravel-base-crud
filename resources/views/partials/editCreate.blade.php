5b10ea0ded20aae6800ae67933ebffd22baa0e78
andmalt <andrea.maltempi@gmail.com>
2021-11-08 23:24:52 +0100

    partial inserted edit create page

--- resources/views/partials/editCreate.blade.php
+++ resources/views/partials/editCreate.blade.php
@@ -9,7 +9,29 @@
                 </div>
                 <div class="card-body text-dark">
                     <form action="{{  $request->routeIs('comics.edit') ? route('comics.update', $comic) : route('comics.store') }}" method="post">
-                        
+                        @csrf
+                        <div class="row">
+                            <div class="col-12 col-md-10 col-lg-9 d-flex justify-content-between align-items-center ">
+                                <label for="title" class="form-label col-12 col-md-8 col-lg-3">Titolo fumetto</label>
+                                <input class="form-control " type="text" id="title" name="title" placeholder="Titolo" value="{{ $comic->title }}" required>
+                                <div class="form-text m-3 col-12 col-md-10 col-lg-4">Inserisci il titolo del nuovo fumetto</div>       
+                            </div>
+                            <div class="col-12 col-md-10 col-lg-9 d-flex justify-content-between align-items-center ">
+                                <label for="author" class="form-label col-12 col-md-8 col-lg-3">Autore fumetto</label>
+                                <input class="form-control " type="text" id="author" name="author" placeholder="Autore" value="{{ $comic->author }}" required>
+                                <div class="form-text m-3 col-12 col-md-10 col-lg-4">Inserisci l'autore del nuovo fumetto</div>       
+                            </div>
+                            <div class="col-12 col-md-10 col-lg-9 d-flex justify-content-between align-items-center ">
+                                <label for="image" class="form-label col-12 col-md-8 col-lg-3">Immagine fumetto</label>
+                                <input class="form-control " type="text" id="image" name="image" placeholder="Immagine" value="{{ $comic->image_url }}" required>
+                                <div class="form-text m-3 col-12 col-md-10 col-lg-4">Inserisci immagine link</div>       
+                            </div>
+                            <div class="col-12 col-md-10 col-lg-9 d-flex justify-content-between align-items-center ">
+                                <label for="image" class="form-label col-12 col-md-8 col-lg-3">Immagine fumetto</label>
+                                <input class="form-control " type="text" id="image" name="image" placeholder="Immagine" value="{{ $comic->image_url }}" required>
+                                <div class="form-text m-3 col-12 col-md-10 col-lg-4">Inserisci immagine link</div>       
+                            </div>
+                        </div>
                     </form>
                 </div>
               </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar_h p-3">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Comics Fanpage</a>
+      <a class="navbar-brand" href="{{route('comics.index')}}">Comics Fanpage</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
