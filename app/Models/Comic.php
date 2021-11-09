276739ac190f856f9f4c42997bf95259d194cd4c
andmalt <andrea.maltempi@gmail.com>
2021-11-09 15:42:14 +0100

    inserted soft delete

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comic extends Model
 {
+    use SoftDeletes;
+    
     protected $fillable = ['title', 'author', 'image_url','publication_date','description'];
 }
